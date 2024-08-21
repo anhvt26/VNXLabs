c0419e5e53aab336ebca52f921bae1b8150a7c55
anhvt26 <tuananh26ttvt@gmail.com>
2024-08-21 22:56:43 +0700

    initial

---
+++ VNXLabs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VNXLabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
